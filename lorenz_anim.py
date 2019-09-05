9c396a3ebc90ad0d67ba61799d6c42a2a851cedd
Aymen Hafeez <49293546+aymenhafeez@users.noreply.github.com>
2019-09-05 03:12:18 +0100

    Update lorenz_anim.py

--- lorenz_anim.py
+++ lorenz_anim.py
@@ -1,10 +1,9 @@
-from matplotlib.pyplot import style
-from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 # from matplotlib.pyplot import style
-from scipy.integrate import odeint
-import numpy as np
 
 # style.use('dark_background')
 
@@ -12,27 +11,28 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 lines = sum(
-    [ax.plot([], [], [], color=(0.133, 0.627, 0.564), lw=0.25, alpha=0.5)], [])
+    [ax.plot([], [], [], c=(0.133, 0.627, 0.564), lw=0.5, alpha=0.5)], [])
+
 points = sum(
-    [ax.plot([], [], [], 'o', color=(0.847, 0.392, 0.274),
-             markerfacecolor='None', markersize=4, markeredgewidth=0.25)], [])
+    [ax.plot([], [], [], 'o', c=(0.847, 0.392, 0.274), markerfacecolor='None', 
+             markersize=4, markeredgewidth=0.25)], [])
 
 sigma = 10
 rho = 28
 beta = 8/3
-x0 = [0, 1, 15]
-t_s, t_m = 0, 100
-t = np.linspace(t_s, t_m, 25000)
+x0 = [0, 1, 20]
+t_s, t_m = 0, 150
+t = np.linspace(t_s, t_m, 50000)
 
 
-def lorenz(x, t):
-    x1 = x[0]
-    y = x[1]
-    z = x[2]
+def lorenz(x_var, t):
+    x = x_var[0]
+    y = x_var[1]
+    z = x_var[2]
 
-    dx_dt = sigma * (y - x1)
-    dy_dt = x1 * (rho - z) - y
-    dz_dt = x1 * y - beta * z
+    dx_dt = sigma * (y - x)
+    dy_dt = x * (rho - z) - y
+    dz_dt = x * y - beta * z
 
     return [dx_dt, dy_dt, dz_dt]
 
@@ -73,6 +73,5 @@ ax.set_zlim((0, 40))
 ax.set_axis_off()
 ax.grid(False)
 
-anim = animation.FuncAnimation(
-    fig, animate, init_func=init, frames=25000, interval=1)
+anim = animation.FuncAnimation(fig, animate, init_func=init, interval=1)
 plt.show()
